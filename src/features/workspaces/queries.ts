bdc6e0b3d6b011656302563f5c38b9b6580a65c4
thuong.tran1 <thuong.tran1@ntq-solution.com.vn>
2026-07-30 16:20:25 +0700

    fix: 410 workspace

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,12 +1,25 @@
 import { client } from "@/lib/rpc";
 import { Workspace } from "./type";
+import { cookies } from "next/headers";
 
 export interface GetWorkspacesResponse {
     data: Workspace[];
 }
-
 export const getWorkspaces = async () => {
-    const response = await client.api.workspaces.$get();
+    const cookieStore = await cookies();
+
+    const response = await client.api.workspaces.$get(
+        {},
+        {
+            headers: {
+                Cookie: cookieStore.toString(),
+            },
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch workspaces");
+    }
 
     const { data }: GetWorkspacesResponse = await response.json() as GetWorkspacesResponse;
 
